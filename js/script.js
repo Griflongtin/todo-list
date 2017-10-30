8faa6d0ec9b6a18ce3afa8e32b78fbf29d77e33f
Ginger Lee Kretschmer and Griffin P Longtin <ginger@>
2017-10-30 16:26:36 -0700

    add remove checkbox

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Business Logic
-function Tasks(name, notes, dueDate) {
+function Tasks(name, category, notes, dueDate) {
   this.name = name;
-  // this.category = "";
+  this.category = category;
   this.notes = notes;
   this.dueDate = dueDate;
 }
@@ -10,22 +10,27 @@ function Tasks(name, notes, dueDate) {
 $(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-      debugger;
     var taskName = $("#name").val();
+    var taskCategory = $("#category").val();
     var taskNotes = $("#notes").val();
     var taskDue = $("#due").val();
 
-    var todoItem = new Tasks(taskName, taskNotes, taskDue);
+    var todoItem = new Tasks(taskName, taskCategory, taskNotes, taskDue);
 
-    $("#output-task-name").append('<li class="list-group-item clickable">' + todoItem.name + '</li>');
+    $("#output-task-name").append('<li class="list-group-item">' + '<span class="clickable float-left">' + todoItem.name + '</span>' + '<div class="form-check form-check-inline clickable-remove float-right"><label class="form-check-label"><input class="form-check-input" type="checkbox">Done</label></div>' + '</li>');
 
     $(".clickable").last().click(function() {
       $(".results").show();
 
       $("#selectName").text(todoItem.name);
-      // $("#selectCategory").text(todoItem.category);
+      $("#selectCategory").text(todoItem.category);
       $("#selectNotes").text(todoItem.notes);
       $("#selectDue").text(todoItem.dueDate);
     });
+
+    $(".clickable-remove").click(function() {
+      $(".results").hide();
+      $(this).parent().remove();
+    })
   });
 });
